5be4a265a5b94b218c913d58f14c48ef4ee00b7f
Sam Skinner <sam.sjskinner@gmail.com>
2021-02-04 11:16:54 +1100

    added websockets and chat requirments

--- controllers/projectsController.js
+++ controllers/projectsController.js
@@ -1,4 +1,5 @@
 const Project = require('../models/Project')
+const User = require('../models/User')
 
 module.exports = {
   // Project routes
@@ -8,6 +9,44 @@ module.exports = {
       res.json(projects);
     } catch(error) {
       console.log('ERROR querying DB for projects index');
+      res.sendStatus(500);
+    }
+  },
+
+  async show(req, res) {
+    try {
+      const project = await Project.findOne({_id: req.params.id})
+      .populate('projectLead')
+      .populate('projectMembers');
+      res.json(project);
+    } catch(error) {
+      console.log('ERROR querying DB for project show');
+      res.sendStatus(500);
+    }
+  },
+
+  async create(req, res) {
+    try {
+      const project = await Project.create({
+        name: req.body.name,
+        description: req.body.description,
+        catagory: req.body.catagory,
+        projectLead: req.user._id,
+        projectMembers: [],
+        lookingFor: req.body.lookingFor,
+        image: req.body.image
+      });
+      const user = await User.findOneAndUpdate(
+        {_id: req.user._id},
+        {
+          $push: {
+            leadOn: project._id
+          }
+        }
+      );
+      res.json(project);
+    } catch(error) {
+      console.log('ERROR creating new project');
       res.sendStatus(500);
     }
   }
--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const projectsController = require('./controllers/projectsController');
 const usersController = require('./controllers/usersController');
 const cors = require('cors');
 const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, {
+  cors: {
+    origin: "*"
+  }
+});
 
 // Authentication
 const User = require('./models/User');
@@ -35,17 +41,39 @@ mongoose.connect(mongoDB, {
 const db = mongoose.connection
 db.on('error', err => console.log('Connection error:', err));
 
+// Setup websocket connection & handle chat
+io.on('connection', socket => {
+  const NEW_CHAT_MESSAGE_EVENT = 'newChatMessage';
+  console.log('user connected');
+  // Join a conversation
+  const {roomId} = socket.handshake.query;
+  socket.join(roomId);
+
+  // Listen for new messages
+  socket.on(NEW_CHAT_MESSAGE_EVENT, data => {
+    io.in(roomId).emit(NEW_CHAT_MESSAGE_EVENT, data);
+  });
+
+  // Leave the room if the user closes the socket
+  socket.on('disconnect', () => {
+    socket.leave(roomId);
+  });
+});
+
 // Start listening on port 8000
-const server = app.listen(8000, () => {
+// const server = app.
+server.listen(8000, () => {
   console.log('Listening at 127.0.0.1:8000...');
 });
 
 // Route endpoints
-app.get('/flights', projectsController.index);
-app.post('/users/create', usersController.create);
 app.get('/user', checkAuth(), usersController.show);
+app.post('/users/create', usersController.create);
 app.patch('/user/update', checkAuth(), usersController.update);
 app.post('/user/delete', checkAuth(), usersController.delete);
+app.get('/projects', projectsController.index);
+app.get('/project/:id', projectsController.show);
+app.post('/projects/create', checkAuth(), projectsController.create);
 
 // Authentication route
 app.post('/login', usersController.login);
